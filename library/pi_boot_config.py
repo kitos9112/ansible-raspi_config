07ec7d4773d3fecdfb3e4ea055a4030535643e66
Marcos Soutullo Rodriguez <marcos.soutullo91@gmail.com>
2024-07-21 21:30:51 +0100

    fix: Get rid of legacy timesyncd service
    Tweak some Python scripts with autoformatting and use hyphen for the new raspi-config command

--- library/pi_boot_config.py
+++ library/pi_boot_config.py
@@ -15,7 +15,7 @@ class ConfigFile:
 
     @staticmethod
     def __param_string(param, value):
-        return param+"="+value
+        return param + "=" + value
 
     def __init__(self, file_name=BOOT_CONFIG_PATH):
         self.is_changed = False
@@ -26,7 +26,9 @@ class ConfigFile:
 
     def __find_starting_with(self, searched):
         try:
-            return [x.find(self.__param_string(searched, "")) == 0 for x in self.lines].index(True)
+            return [
+                x.find(self.__param_string(searched, "")) == 0 for x in self.lines
+            ].index(True)
         except ValueError:
             return -1
 
@@ -34,31 +36,32 @@ class ConfigFile:
         # search for an uncommented line, and a commented one if that fails
         line_num = self.__find_starting_with(param)
         if line_num == -1:
-            line_num = self.__find_starting_with('#'+param)
+            line_num = self.__find_starting_with("#" + param)
 
         # ...and finally just create an empty line
         if line_num == -1:
             line_num == len(self.lines)
             self.lines.append("")
 
-        target_value = self.__param_string(param, value)+"\n"
+        target_value = self.__param_string(param, value) + "\n"
         if self.lines[line_num] != target_value:
             self.lines[line_num] = target_value
             self.is_changed = True
-            with open(self.file_name, 'w') as fp:
+            with open(self.file_name, "w") as fp:
                 fp.writelines(self.lines)
             return True
         else:
             return False
+
+
 # END - common module code - yay for copy-paste
 
 CONFIG_VALS = "config_vals"
 
 
 def main():
-    module = AnsibleModule(argument_spec={
-        CONFIG_VALS: {"required": True, "type": "dict"}
-    }
+    module = AnsibleModule(
+        argument_spec={CONFIG_VALS: {"required": True, "type": "dict"}}
     )
 
     config_vals = module.params.get(CONFIG_VALS)
@@ -71,15 +74,15 @@ def main():
     # sanitize from auto-typing in YAML
     config_vals = dict((str(key), str(val)) for (key, val) in list(config_vals.items()))
 
-    for (key, val) in list(config_vals.items()):
+    for key, val in list(config_vals.items()):
         try:
             modified = config.set(key, val)
             if modified:
-                out += "\nModified "+key+" to "+val
+                out += "\nModified " + key + " to " + val
             else:
-                out += "\n"+key+" was already set to "+val
+                out += "\n" + key + " was already set to " + val
         except Exception as e:
-            err = "Error when writing config: "+str(e)
+            err = "Error when writing config: " + str(e)
             module.fail_json(changed=config.is_changed, msg=err, stdout=out, stderr=err)
 
     module.exit_json(changed=config.is_changed, stdout=out, stderr=err)
